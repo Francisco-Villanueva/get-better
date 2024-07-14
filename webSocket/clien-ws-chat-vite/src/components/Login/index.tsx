3b327836272c73ab71c7cd43048d1e5aa764f51b
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2024-07-14 18:54:44 -0300

    add websocket

---
+++ webSocket/clien-ws-chat-vite/src/components/Login/index.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { Button } from "../ui/button";
+
+export function Login({
+  setUsername,
+}: {
+  setUsername: (username: string) => void;
+}) {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    setUsername(inputValue);
+  };
+
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <form onSubmit={handleSubmit} className="flex flex-col">
+        <input
+          type="text"
+          placeholder="Enter your username"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          className="p-2 border rounded mb-4"
+        />
+        <Button type="submit">Join Chat</Button>
+      </form>
+    </div>
+  );
+}
