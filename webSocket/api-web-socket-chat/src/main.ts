26e15f4fab22f1f5a6d51eb9133685d3411310b1
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2024-07-14 19:29:53 -0300

    delete cors instantly

--- webSocket/api-web-socket-chat/src/main.ts
+++ webSocket/api-web-socket-chat/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://wechat-ws.vercel.app/'], // Aquí especificas la URL de tu frontend
+    origin: '*', // Aquí especificas la URL de tu frontend
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
